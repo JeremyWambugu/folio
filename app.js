a063940dffcd534aca87dbfffe500722ec6f2bbf
jeremy wambugu <wambugujeremy@gmail.com>
2020-06-08 18:19:24 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+function smoothScroll(target, duration) {
+    var target = document.querySelector(target)
+    var targetPosition = target.getBoundingClientRect()
+}
+smoothScroll('#contact', 2000)
